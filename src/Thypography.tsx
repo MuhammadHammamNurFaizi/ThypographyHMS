9394c9949f6f8b61cef1c46e6ae219037939c2f7
unknown <faizihammam@gmail.com>
2022-09-07 23:21:14 +0700

    first commit

---
+++ src/Thypography.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import './App.css';
+
+function Thypography() {
+  return (
+    <div className="Thypography">
+      <h1>This is h1</h1>
+      <h2>This is h2</h2>
+      <h3>This is h3</h3>
+      <h4>This is h4</h4>
+      <h5>This is h5</h5>
+      <h6>This is h6</h6>
+    </div>
+  );
+}
+
+export default Thypography;
